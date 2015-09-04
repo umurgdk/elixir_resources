b2f9ad56c47338cb797fe80fa31c03c7882daa39
Emir Alp <okebye@gmail.com>
2015-09-04 05:12:14 +0300

    looping rss titles

--- workers/rss_worker.ex
+++ workers/rss_worker.ex
@@ -1,6 +1,16 @@
 defmodule ElixirResources.Workers.RSSWorker do
   def perform do
-    # TODO: Rss
+    
+		# TODO: Rss
+		
     IO.puts "Worker fetching rss streams..."
+		
+		HTTPoison.start
+		
+		{:ok, %HTTPoison.Response{body: body}} = HTTPoison.get("http://www.rssboard.org/files/sample-rss-2.xml")
+		{:ok, feed, _} = FeederEx.parse(body)
+		
+		Enum.map feed.entries, fn(entry) -> IO.puts(entry.title) end
+		
   end
 end
